fa60d379d9df7e1d7a4289cca3f204bbca8503b1
Ankit Kannaujiya <ankitkannaujiya666@gmail.com>
2022-04-11 23:46:15 +0530

    Validation code in Registration View

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './component/Navbar';
+
+import RegistrationView from './component/RegistrationView';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <RegistrationView/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container">
+    <a className="navbar-brand" href="#">Scroll Effects</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+          
+        <li className="nav-item mx-2">
+            <button type="button" className=" btn btn-outline-success">
+          <a className="nav-link" aria-current="page" href="#">Scroll UP &uarr; </a>
+        </button>
+        </li>
+
+        <li className="nav-item"> 
+        <button type="button" className="btn btn-outline-danger">
+          <a className="nav-link" href="#">Scroll Down &darr;</a>
+        </button>
+        </li>
+      </ul>
+    
+    </div>
+  </div>
+</nav>
+        </div>
+    )
+}
+
+export default Navbar
